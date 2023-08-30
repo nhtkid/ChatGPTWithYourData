c51b654c4d0b566d2b29c4628cfec560f22a734f
nhtkid <93020586+nhtkid@users.noreply.github.com>
2023-08-30 12:35:46 +1000

    Update ChatWithYourData_v2.py

--- ChatWithYourData_v2.py
+++ ChatWithYourData_v2.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import OpenAIWhisperParser, OpenAIWhisperParserLocal
+from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import UnstructuredFileLoader
@@ -65,26 +68,33 @@ if uploaded_files or youtube_url:
 
         # Load the YouTube audio stream if URL is provided
         if youtube_url:
-            # Load the Whisper model
-            # model = whisper.load_model('base')
-            # Download the audio stream
-            youtube_video = pytube.YouTube(youtube_url)
-            streams = youtube_video.streams.filter(only_audio=True)
-            stream = streams.first()
-            stream.download(filename="youtube_audio.mp4")
-            # Transcribe the audio using OpenAI Whisper API
-            # output = model.transcribe("youtube_audio.mp4")
-            # Transcribe the audio using OpenAI Whisper API
-            with open("youtube_audio.mp4", "rb") as audio_file:
-                transcript = openai.Audio.transcribe("whisper-1", audio_file)
-            youtube_text = transcript['text']
+                # Transcribe YouTube audio to text
+                local = False  # Set this to True for local parsing
+
+                # Create a list with the provided YouTube URL
+                urls = [youtube_url]
+            
+                # Directory to save audio files
+                save_dir = "~/Downloads/YouTube"
+            
+                # Transcribe the videos to text
+                if local:
+                    loader = GenericLoader(YoutubeAudioLoader(urls, save_dir), OpenAIWhisperParserLocal())
+                else:
+                    loader = GenericLoader(YoutubeAudioLoader(urls, save_dir), OpenAIWhisperParser())
+                docs = loader.load()
+            
+                # Combine doc content
+                combined_docs = [doc.page_content for doc in docs]
+                text = " ".join(combined_docs)
+            
             # Create a Langchain document instance for the transcribed text
             from langchain.documents import Document
             youtube_document = Document(page_content=youtube_text, metadata={})
             documents.append(youtube_document)
 
         # Chunk the data, create embeddings, and save in vectorstore
-        text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
         document_chunks = text_splitter.split_documents(documents)
 
         embeddings = OpenAIEmbeddings()
