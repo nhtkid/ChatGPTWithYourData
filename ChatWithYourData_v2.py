20ddb8b78b6d56d211fbf439d55a782381daba1a
nhtkid <93020586+nhtkid@users.noreply.github.com>
2023-09-17 21:58:25 +1000

    Update ChatWithYourData_v2.py

--- ChatWithYourData_v2.py
+++ ChatWithYourData_v2.py
@@ -14,45 +14,50 @@ import openai
 
 # Chat UI title
 st.header("Upload your own files and ask questions like ChatGPT")
-st.subheader('File type supported: PDF/DOCX/TXT/JPG/PNG/YouTube :city_sunrise:')
+st.subheader('File types supported: PDF/DOCX/TXT/JPG/PNG/YouTube :city_sunrise:')
 
 # File uploader in the sidebar on the left
 with st.sidebar:
+    # Input for OpenAI API Key
     openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.")
-    st.stop()
 
-# Set OPENAI_API_KEY as an environment variable
-os.environ["OPENAI_API_KEY"] = openai_api_key
+    # Check if OpenAI API Key is provided
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
 
-llm = ChatOpenAI(temperature=0,max_tokens=1000, model_name="gpt-3.5-turbo",streaming=True)
+    # Set OPENAI_API_KEY as an environment variable
+    os.environ["OPENAI_API_KEY"] = openai_api_key
+
+# Initialize ChatOpenAI model
+llm = ChatOpenAI(temperature=0, max_tokens=1000, model_name="gpt-3.5-turbo", streaming=True)
 
 # Load version history from the text file
 def load_version_history():
     with open("version_history.txt", "r") as file:
         return file.read()
-       
+
+# Sidebar section for uploading files and providing a YouTube URL
 with st.sidebar:
     uploaded_files = st.file_uploader("Please upload your files", accept_multiple_files=True, type=None)
-    youtube_url = st.sidebar.text_input("YouTube URL")
-    # st.info(load_version_history(), icon="🤖")
-    
+    youtube_url = st.text_input("YouTube URL")
+
     # Create an expander for the version history in the sidebar
     with st.sidebar.expander("**Version History**", expanded=False):
         st.write(load_version_history())
 
-    st.info("Please refresh the browser if you decided to upload more files to reset the session", icon="🚨")
+    st.info("Please refresh the browser if you decide to upload more files to reset the session", icon="🚨")
 
-# Check if files are uploaded or YT URL is provided
+# Check if files are uploaded or YouTube URL is provided
 if uploaded_files or youtube_url:
-    # Print the number of files uploaded or YouTube URL provided to console
+    # Print the number of files uploaded or YouTube URL provided to the console
     st.write(f"Number of files uploaded: {len(uploaded_files)}")
-      
+
     # Load the data and perform preprocessing only if it hasn't been loaded before
     if "processed_data" not in st.session_state:
-        # Load the data from uploaded PDF files
+        # Load the data from uploaded files
         documents = []
+
         if uploaded_files:
             for uploaded_file in uploaded_files:
                 # Get the full file path of the uploaded file
@@ -62,28 +67,22 @@ if uploaded_files or youtube_url:
                 with open(file_path, "wb") as f:
                     f.write(uploaded_file.getvalue())
 
-                 # Check if the file is an image (you can adjust this condition as needed)
-               #if file_path.endswith((".png", ".jpg")):
-                    # Use UnstructuredImageLoader to load the image file
-               #    image_loader = UnstructuredImageLoader(file_path, mode="single")
-                    # Use [0] to get the single Langchain document
-               #    image_data = image_loader.load()[0]
-                #    print(image_data)
-                    # Append the Langchain document to the documents list
-                #   documents.extend(image_data)
-
+                # Check if the file is an image
                 if file_path.endswith((".png", ".jpg")):
                     # Use ImageCaptionLoader to load the image file
-                   image_loader = ImageCaptionLoader(path_images=[file_path])
+                    image_loader = ImageCaptionLoader(path_images=[file_path])
+
                     # Load image captions
-                   image_documents = image_loader.load()
+                    image_documents = image_loader.load()
+
                     # Append the Langchain documents to the documents list
-                   documents.extend(image_documents)
+                    documents.extend(image_documents)
                     
                 elif file_path.endswith((".pdf", ".docx", ".txt")):
                     # Use UnstructuredFileLoader to load the PDF/DOCX/TXT file
                     loader = UnstructuredFileLoader(file_path)
                     loaded_documents = loader.load()
+
                     # Extend the main documents list with the loaded documents
                     documents.extend(loaded_documents)
 
@@ -96,6 +95,7 @@ if uploaded_files or youtube_url:
             with open("youtube_audio.mp4", "rb") as audio_file:
                 transcript = openai.Audio.transcribe("whisper-1", audio_file)
             youtube_text = transcript['text']
+
             # Create a Langchain document instance for the transcribed text
             youtube_document = Document(page_content=youtube_text, metadata={})
             documents.append(youtube_document)
@@ -103,9 +103,6 @@ if uploaded_files or youtube_url:
         # Chunk the data, create embeddings, and save in vectorstore
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
         document_chunks = text_splitter.split_documents(documents)
-        # Print the number of total chunks to console
-        #st.write(f"Number of total chunks: {len(document_chunks)}")
-        #print (document_chunks)
 
         embeddings = OpenAIEmbeddings()
         vectorstore = Chroma.from_documents(document_chunks, embeddings)
@@ -122,7 +119,7 @@ if uploaded_files or youtube_url:
         vectorstore = st.session_state.processed_data["vectorstore"]
 
     # Initialize Langchain's QA Chain with the vectorstore
-    qa = ConversationalRetrievalChain.from_llm(llm,vectorstore.as_retriever())
+    qa = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever())
 
     # Initialize chat history
     if "messages" not in st.session_state:
